e911d850f164b5ef1531ad687beac7e488b4cacc
Alessandro Suha <plarpoon@plarp.net>
2023-09-24 04:36:35 +0200

    change version of nix channel and add nur

--- flake.nix
+++ flake.nix
@@ -9,16 +9,26 @@
 
   inputs =                                                                  # References Used by Flake
     {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";                     # Stable Nix Packages (Default)
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";                     # Stable Nix Packages (Default)
       nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";         # Unstable Nix Packages
 
       home-manager = {                                                      # User Environment Manager
-        url = "github:nix-community/home-manager/release-23.11";
+        url = "github:nix-community/home-manager/release-23.05";
         inputs.nixpkgs.follows = "nixpkgs";
+      };
+
+      nur = {                                                               # NUR Community Packages
+        url = "github:nix-community/NUR";                                   # Requires "nur.nixosModules.nur" to be added to the host modules
+      };
+
+      plasma-manager = {                                                    # KDE Plasma User Settings Generator
+        url = "github:pjones/plasma-manager";                               # Requires "inputs.plasma-manager.homeManagerModules.plasma-manager" to be added to the home-manager.users.${user}.imports
+        inputs.nixpkgs.follows = "nixpkgs";
+        inputs.home-manager.follows = "nixpkgs";
       };
     };
 
-  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:   # Function telling flake which inputs to use
+  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, nur, home-manager, ... }:   # Function telling flake which inputs to use
     let
       vars = {                                                              # Variables Used In Flake
         user = "plarpoon";
@@ -29,7 +39,7 @@
       nixosConfigurations = (                                               # NixOS Configurations
         import ./hosts {
           inherit (nixpkgs) lib;
-          inherit inputs nixpkgs nixpkgs-unstable home-manager vars;   # Inherit inputs
+          inherit inputs nixpkgs nixpkgs-unstable home-manager nur vars;   # Inherit inputs
         }
       );
     };
