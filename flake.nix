0804f0e6c481576abe5c5190b67a8e0badfce2fb
Alessandro Suha <plarpoon@plarp.net>
2023-10-01 04:14:40 +0200

    add configuration files for hosts bjorn and daisy

--- flake.nix
+++ flake.nix
@@ -1,4 +1,7 @@
 {
+  username = "plarpoon";
+  stateVersion = "23.11";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager = {
@@ -6,17 +9,40 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = inputs @ {
-    home-manager,
-    nixpkgs,
-    ...
-  }: {
-    homeConfigurations."plarpoon" = home-manager.lib.homeManagerConfiguration {
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    homeConfigurations.${self.username} = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       modules = [
         ./home.nix
       ];
       extraSpecialArgs = {inherit inputs;};
+    };
+
+    nixosConfigurations = {
+      bjorn = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/bjorn/configuration.nix
+          self.inputs.home-manager.nixosModules.home-manager
+          { 
+            home-manager.users.${self.username} = self.homeConfigurations.${self.username}; 
+            system.stateVersion = self.stateVersion;
+          }
+        ];
+      };
+
+      daisy = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/daisy/configuration.nix
+          self.inputs.home-manager.nixosModules.home-manager
+          { 
+            home-manager.users.${self.username} = self.homeConfigurations.${self.username}; 
+            system.stateVersion = self.stateVersion;
+          }
+        ];
+      };
     };
   };
 }
