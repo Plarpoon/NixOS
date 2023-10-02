38d9552eb75ee7ac16a91f85904f2f8def14e3aa
Alessandro Suha <plarpoon@plarp.net>
2023-10-02 04:08:42 +0200

    change the way systemd-resolved settings are applied

--- hosts/bjorn/configuration.nix
+++ hosts/bjorn/configuration.nix
@@ -45,25 +45,26 @@
     };
   };
 
-  # Systemd services
-  systemd = {
-    packages = [ pkgs.systemd ];
+  networking.nameservers = [ 
+    "45.90.28.0#bjorn-e38da2.dns.nextdns.io" 
+    "2a07:a8c0::#bjorn-e38da2.dns.nextdns.io" 
+    "45.90.30.0#bjorn-e38da2.dns.nextdns.io" 
+    "2a07:a8c1::#bjorn-e38da2.dns.nextdns.io" 
+  ];
 
-    # Systemd-resolved configuration
-    resolved = {
-      enable = true;
-      settings = {
-        Resolve = {
-          DNS = [
-            "45.90.28.0#bjorn-e38da2.dns.nextdns.io"
-            "2a07:a8c0::#bjorn-e38da2.dns.nextdns.io"
-            "45.90.30.0#bjorn-e38da2.dns.nextdns.io"
-            "2a07:a8c1::#bjorn-e38da2.dns.nextdns.io"
-          ];
-          DNSOverTLS = "yes";
-        };
-      };
-    };
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ 
+      "45.90.28.0#bjorn-e38da2.dns.nextdns.io" 
+      "2a07:a8c0::#bjorn-e38da2.dns.nextdns.io" 
+      "45.90.30.0#bjorn-e38da2.dns.nextdns.io" 
+      "2a07:a8c1::#bjorn-e38da2.dns.nextdns.io" 
+    ];
+    extraConfig = ''
+      DNSOverTLS=yes
+    '';
   };
 
   ## SSH
